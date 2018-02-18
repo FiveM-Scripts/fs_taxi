47498ee37752df96bedf786ac3483703e09276de
ghermans <glhermans@gmail.com>
2018-02-19 00:33:08 +0100

    Added translation files

---
+++ locales/en.lua
@@ -0,0 +1,8 @@
+i18n.importData("en", {
+	taxi_contact = "Taxi call",
+	welcome_message = "Welcome, this is a test from i18n",
+	dispatch_message = "The taxi driver is on his way",
+	drivers_busy = "All our drivers are currently occupied.",
+	info_message = "Press ~INPUT_PICKUP~ to drive as a passenger or ~INPUT_ENTER~ to steal the vehicle.",
+	taxi_dispatch = "The taxi cab is on his way."
+})
